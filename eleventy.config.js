89eb0170a58705bd9752daf920cade77acb8a469
Ravan Guliyev <guliyevravan@gmail.com>
2025-06-05 11:11:44 +0400

    Fayllar hazırlandı

---
+++ eleventy.config.js
@@ -0,0 +1,12 @@
+export default async function (eleventyConfig) {
+
+  eleventyConfig.setInputDirectory('src'); 
+  eleventyConfig.setIncludesDirectory("_includes");
+  eleventyConfig.setLayoutsDirectory("_layouts");
+  eleventyConfig.setDataDirectory("_data");
+  eleventyConfig.setOutputDirectory("_site");
+
+  eleventyConfig.addPassthroughCopy("./src/assets/css/style.css");
+  eleventyConfig.addPassthroughCopy("./src/assets/img");
+
+}
