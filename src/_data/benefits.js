076115c754d4a96b05cb6ffeea827d7916b13141
Ravan Guliyev <ravan.guliyev@acha.local>
2025-06-20 12:11:40 +0400

    Projects bölməsi əlavə edildi

---
+++ src/_data/benefits.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    title: 'You get a product with a business value',
+    description: 'Our teams don’t just build software, we solve your business problems',
+  },
+  {
+    title: 'Expert-Vetted Talent',
+    description: 'We carefully vet candidates through assessments, technical challenges, and interviews to hire only the best',
+  },
+  {
+    title: 'Expertise in over 40 technologies',
+    description: 'We\'ve explored a lot of tools, technologies and services on the market to bring your business the best solution',
+  },
+  {
+    title: 'We work with companies from S&P 500',
+    description: 'Proven track record with clients across the USA, UK, and Europe',
+  },
+  {
+    title: 'Custom Software Solutions',
+    description: 'We deliver end-to-end custom development solutions tailored to businesses of all sizes',
+  },
+  {
+    title: 'We’re invested in your success as true partners',
+    description: 'Our solutions empower your business to win in today\'s tech-driven market',
+  },
+  {
+    title: 'Faster Time to Market',
+    description: 'Our approach allows for quick testing of assumptions, ultimately leading to faster time to market and significant savings in resources',
+  },
+  {
+    title: 'Business Growth Support',
+    description: 'We handle the logistics of building dedicated teams of software developers, allowing you to focus solely on your product, delivery, sales, and business vision',
+  },
+  {
+    title: 'Agile Development',
+    description: 'We employ an Agile approach to reduce risks, increase flexibility, improve control and customer focus, and build superior quality products with improved project predictability',
+  },
+]
---
+++ src/_data/projects.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    img: 'https://cdn.prod.website-files.com/669e317d79600bcb185a8939/67acc50038d4826c0607d284_aobp.png',
+    title: 'Defense and Security Industry Association of the Czech Republic',
+    url: 'https://aobp.cz/'
+  },
+  {
+    img: 'https://cdn.prod.website-files.com/669e317d79600bcb185a8939/67bf613e5a9a49d7982036de_czech-association-ai.png',
+    title: 'Czech Association of Artificial Intelligence',
+    url: 'https://asociace.ai/'
+  },
+  {
+    img: 'https://cdn.prod.website-files.com/669e317d79600bcb185a8939/67c9a03de0808f71cfda0cfb_czech-hispanic-chamber.png',
+    title: 'Czech-Hispanic Chamber of Commerce',
+    url: 'https://camaracomerciohispanocheca.eu/'
+  },
+  {
+    img: 'https://cdn.prod.website-files.com/669e317d79600bcb185a8939/67d04cf23ce353a1a55c842a_vrije-universiteit.png',
+    title: 'Vrije Universiteit Brussel',
+    url: 'https://www.vub.be/en'
+  },
+  {
+    img: 'https://cdn.prod.website-files.com/669e317d79600bcb185a8939/67d04cf2f7572a602ce70d7d_technical-university%20-ostrava.png',
+    title: 'Technical University of Ostrava',
+    url: 'https://www.vsb.cz/en'
+  },
+  {
+    img: 'https://cdn.prod.website-files.com/669e317d79600bcb185a8939/67d04cf2a1bdfbab9689142e_san-diego-state-university.png',
+    title: 'San Diego State University',
+    url: 'https://www.sdsu.edu/'
+  },
+]
